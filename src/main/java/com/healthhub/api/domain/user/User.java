a05f2558df93b56cf50fb07e836062dd713ca9f2
Dimitri <dimitri.s.amado@gmail.com>
2026-03-19 14:00:30 -0300

    feat: Implement basic structure and configuration

---
+++ src/main/java/com/healthhub/api/domain/user/User.java
@@ -0,0 +1,27 @@
+package com.healthhub.api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+    private String email;
+}
